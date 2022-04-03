d9837fb1def1fd190a8979eed9683125373508ce
Suraj Virkar <surajvirkar6@gmail.com>
2022-04-03 13:24:08 +0530

    push to gh-page 1

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ import "./Navbar.css";
 function Navbar() {
   return (
     <div className="navbar">
-      <Link to="/" className="link">
+      <Link to="/Shopping-Cart-App" className="link">
         <h1 className="title">Shopping Cart</h1>
       </Link>
       <Link to="/cart" className="link">
